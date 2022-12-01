13593f487bf77e19a9feec1055231b8b0ee6794d
Lazar Mladnovic <lazeta998@gmail.com>
2022-12-01 14:44:52 +0100

    Displaying Toast notification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
 import http from "./services/httpService";
+import "react-toastify/dist/ReactToastify.css";
 import config from "./consfig.json";
 import "./App.css";
 
@@ -38,7 +40,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await http.delete(config.apiEndpoint + "/" + post.id);
+      await http.delete("s" + config.apiEndpoint + "/" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         alert("This post has already been deleted");
@@ -50,6 +52,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
