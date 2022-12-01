b6c904995400eb8a0b7304d3c5bd0ac3ebaeccfe
Lazar Mladnovic <lazeta998@gmail.com>
2022-12-01 14:33:02 +0100

    Extracting a reusable http service

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
 
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Loging the error", error);
-    alert("An unexpected error occurred");
-  }
-
-  return Promise.reject(error);
-});
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
@@ -22,13 +9,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get("s" + apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -36,8 +23,8 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "updated";
-    await axios.put(apiEndpoint + "/" + post.id, post);
-    // axios.patch(apiEndpoint + '/' + post.id, {title: post.title});
+    await http.put(apiEndpoint + "/" + post.id, post);
+    // http.patch(apiEndpoint + '/' + post.id, {title: post.title});
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     post[index] = { ...post };
@@ -51,7 +38,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
+      await http.delete(apiEndpoint + "/" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         alert("This post has already been deleted");
